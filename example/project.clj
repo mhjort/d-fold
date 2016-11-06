c16c6bdbbcdc07409a1ab335d129422d84f0c4cf
Markus Hjort <markus.hjort@gmail.com>
2016-11-06 16:21:05 +0200

    Add example project skeleton

---
+++ example/project.clj
@@ -0,0 +1,13 @@
+(defproject example "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [d-fold "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-clj-lambda "0.8.1"]]
+  :lambda  {"example" [{:handler "d-fold.LambdaFn" ;Do not change this
+                        :memory-size 1536 ;Do not change this
+                        :timeout 300 ;Do not change this
+                        :function-name "d-fold" ;Do not change this
+                        :region "eu-west-1"}]})
