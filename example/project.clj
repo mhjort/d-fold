269c48d5f1990ef43a19e7f03577201e3063c71c
Markus Hjort <markus.hjort@gmail.com>
2016-11-15 21:42:39 +0200

    Bump lein-clj-lambda plugin

--- example/project.clj
+++ example/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [d-fold "0.1.0-SNAPSHOT"]]
-  :plugins [[lein-clj-lambda "0.8.1"]]
+  :plugins [[lein-clj-lambda "0.8.2"]]
   :lambda  {"example" [{:handler "d-fold.LambdaFn" ;Do not change this
                         :memory-size 1536 ;Do not change this
                         :timeout 300 ;Do not change this
