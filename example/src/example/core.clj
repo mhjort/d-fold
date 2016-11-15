37d661adbe3b01f8982015354de793651b652c66
Markus Hjort <markus.hjort@gmail.com>
2016-11-15 20:14:00 +0200

    Cleanup

--- example/src/example/core.clj
+++ example/src/example/core.clj
@@ -3,7 +3,3 @@
 
 (defn d-sum [items]
   (dfold + ((map inc) +) items {:reduce-nodes 2 :aws-region "eu-west-1"}))
-
-;(d-sum (range 1000))
-
-;(dfold + ((map inc) +) (range 100000) {:reduce-nodes 2 :aws-region "eu-west-1"})
