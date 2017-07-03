d6e9fd92f2a00c29005d7085580fe7dd7707fb45
Markus Hjort <markus.hjort@gmail.com>
2017-07-03 17:13:12 +0300

    Update example

--- example/project.clj
+++ example/project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [d-fold "0.1.0"]]
-  :plugins [[lein-clj-lambda "0.9.0"]]
+  :plugins [[lein-clj-lambda "0.10.2"]]
   :lambda  {"example" [{:handler "d-fold.LambdaFn" ;Do not change this
                         :memory-size 1536 ;Do not change this
                         :timeout 300 ;Do not change this
                         :function-name "d-fold" ;Do not change this
-                        :region "eu-west-1"}]})
+                        }]})
